5637643d083ab3b8b4de3f5df6a37449f17511ea
Bill Russo <bilru@live.com>
2017-06-09 20:58:53 -0400

    update for foreach and todo id get

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+			id: 1, 
+	description: 'Meet mom for lunch',
+	completed: false
+},
+			{
+		id: 2,
+		description: "Go to market",
+		completed: false
+
+},
+{
+	id: 3,
+	description: 'Take the do for a walk',
+	completetd: false
+}];
 
 app.get('/', function(req, res){
-	res.send('Todo API Root');
+	res.send('Todo API Root' + todos.length.toString());
+
+});
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+
+	var todoId = parseInt(req.params.id, 10);
+	var result;
+	
+	//interate over the todos. find match
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			result = todo;
+			
+		}
+	});
+		if(result){
+		res.json(result);
+	}else{
+		res.status(404).send();
+	}
 
 });
 
