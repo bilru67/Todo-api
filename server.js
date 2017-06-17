2889f9bc7347ddffe7c612bb9c81725714eadd8d
Bill Russo <bilru@live.com>
2017-06-17 11:01:56 -0400

    added put to todo

--- server.js
+++ server.js
@@ -81,9 +81,37 @@ app.delete('/todos/:id', function(req, res){
 		}else{
 
 				todos = _.without(todos, result);
-				res.json(result)
+				res.json(result);
 				}
 
+});
+
+//Put /todo/:id
+
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function(){
