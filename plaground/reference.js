c861ccd7c82681ae073600f64554b24458cb2a6d
Bill Russo <bilru@live.com>
2017-06-25 16:32:10 -0400

    add sequelize demo

---
+++ plaground/reference.js
@@ -0,0 +1,27 @@
+var person = {
+	name : "bill",
+	age: 21
+};
+
+var grades = [86, 90];
+
+function updatePerson(obj){
+	/*obj = {
+		name: "bill",
+		age: 50
+	}; */
+
+	obj.age = 50;
+}
+
+//array example
+
+function addGrade(array){
+
+	array = [86, 90, 100];
+
+
+}
+
+addGrade(grades);
+console.log(grades);
